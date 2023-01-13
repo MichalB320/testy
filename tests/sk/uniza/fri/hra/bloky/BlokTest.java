13b070623d68fe7febcf76a795f6773da2fbae7d
micha <miso.bezo@centrum.sk>
2023-01-13 11:42:43 +0100

    BlokTest

--- tests/sk/uniza/fri/hra/bloky/BlokTest.java
+++ tests/sk/uniza/fri/hra/bloky/BlokTest.java
@@ -50,4 +50,116 @@ class BlokTest {
         Dimension velkost = blok.getRectangle().getSize();
         assertEquals(5, velkost.getHeight());
     }
+
+    @Test
+    void zmenVelkost20() {
+        Blok blok = new Blok("blue", 5) {
+            @Override
+            public boolean jeRozbijatelny() {
+                return false;
+            }
+
+            @Override
+            public boolean naPrvyUder() {
+                return false;
+            }
+
+            @Override
+            public boolean jeReady() {
+                return false;
+            }
+
+            @Override
+            public void zmenVelkostPadu(Pad pad) {
+
+            }
+
+            @Override
+            public int pridajbody() {
+                return 0;
+            }
+
+            @Override
+            public void zmenVelkost() {
+
+            }
+        };
+        blok.zmenVelkost(20);
+        Rectangle rect = blok.getRectangle();
+        Dimension velkost = rect.getSize();
+        assertEquals(20, velkost.getHeight());
+    }
+
+    @Test
+    void getStrana() {
+        Blok blok = new Blok("blue", 5) {
+            @Override
+            public boolean jeRozbijatelny() {
+                return false;
+            }
+
+            @Override
+            public boolean naPrvyUder() {
+                return false;
+            }
+
+            @Override
+            public boolean jeReady() {
+                return false;
+            }
+
+            @Override
+            public void zmenVelkostPadu(Pad pad) {
+
+            }
+
+            @Override
+            public int pridajbody() {
+                return 0;
+            }
+
+            @Override
+            public void zmenVelkost() {
+
+            }
+        };
+        assertEquals(5, blok.getStrana());
+    }
+
+    @Test
+    void getY50 () {
+        Blok blok = new Blok("black", 5) {
+            @Override
+            public boolean jeRozbijatelny() {
+                return false;
+            }
+
+            @Override
+            public boolean naPrvyUder() {
+                return false;
+            }
+
+            @Override
+            public boolean jeReady() {
+                return false;
+            }
+
+            @Override
+            public void zmenVelkostPadu(Pad pad) {
+
+            }
+
+            @Override
+            public int pridajbody() {
+                return 0;
+            }
+
+            @Override
+            public void zmenVelkost() {
+
+            }
+        };
+        blok.moveR(25, 50);
+        assertEquals(50, blok.getY());
+    }
 }
